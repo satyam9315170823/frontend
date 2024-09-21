e5c4e51a36703db28df3cd2589bc034faf2fad9e
satyam9315170823 <satyam.kumar@ace.,edu.in>
2024-09-21 10:52:27 +0530

    dddd

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
         <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required/>
         <input onChange={(e)=>setPasword(e.target.value)} value={password} type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required/>
         <div className='w-full flex justify-between text-sm mt-[-8px]'>
-            <p className=' cursor-pointer'>Forgot your password?</p>
+            {/* <p className=' cursor-pointer'>Forgot your password?</p> */}
             {
               currentState === 'Login' 
               ? <p onClick={()=>setCurrentState('Sign Up')} className=' cursor-pointer'>Create account</p>
