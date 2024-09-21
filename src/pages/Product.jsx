247cbe75a70f140d620a3ff857a43860f7c331f9
satyam9315170823 <satyam.kumar@acem.edu.in>
2024-09-21 17:04:38 +0530

    toast

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
 import RelatedProducts from '../components/RelatedProducts';
+import { toast } from 'react-toastify'; // Import toast
 
 const Product = () => {
   const { productId } = useParams();
@@ -31,6 +32,7 @@ const Product = () => {
   const handleAddToCart = () => {
     if (productData && size) {
       addToCart(productData._id, size);
+      toast.success('Item added to cart'); // Show toast notification
     } else {
       console.error('Product data or size is missing');
     }
@@ -47,20 +49,20 @@ const Product = () => {
         {/*---------- Product Images------------- */}
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
-              {
-                productData.image.map((item, index) => (
-                  <img 
-                    onClick={() => setImage(item)} 
-                    src={item} 
-                    key={index} 
-                    className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' 
-                    alt="" 
-                  />
-                ))
-              }
+            {
+              productData.image.map((item, index) => (
+                <img 
+                  onClick={() => setImage(item)} 
+                  src={item} 
+                  key={index} 
+                  className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' 
+                  alt="" 
+                />
+              ))
+            }
           </div>
           <div className='w-full sm:w-[80%]'>
-              <img className='w-full h-auto' src={image} alt="" />
+            <img className='w-full h-auto' src={image} alt="" />
           </div>
         </div>
 
@@ -68,28 +70,28 @@ const Product = () => {
         <div className='flex-1'>
           <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
           <div className='flex items-center gap-1 mt-2'>
-              <img src={assets.star_icon} alt="" className="w-3 5" />
-              <img src={assets.star_icon} alt="" className="w-3 5" />
-              <img src={assets.star_icon} alt="" className="w-3 5" />
-              <img src={assets.star_icon} alt="" className="w-3 5" />
-              <img src={assets.star_dull_icon} alt="" className="w-3 5" />
-              <p className='pl-2'>(122)</p>
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className='pl-2'>(122)</p>
           </div>
           <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
           <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
           <div className='flex flex-col gap-4 my-8'>
-              <p>Select Size</p>
-              <div className='flex gap-2'>
-                {productData.sizes.map((item, index) => (
-                  <button 
-                    onClick={() => setSize(item)} 
-                    className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} 
-                    key={index}
-                  >
-                    {item}
-                  </button>
-                ))}
-              </div>
+            <p>Select Size</p>
+            <div className='flex gap-2'>
+              {productData.sizes.map((item, index) => (
+                <button 
+                  onClick={() => setSize(item)} 
+                  className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} 
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
           </div>
           <button 
             onClick={handleAddToCart} 
